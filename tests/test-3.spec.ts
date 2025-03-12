d3b1857c03e81c65ecc9dd046f11347802c1a8db
Rahul Birla <birlarahul340@gmail.com>
2025-03-12 11:15:49 +0530

    debuge methods practice

--- tests/test-3.spec.ts
+++ tests/test-3.spec.ts
@@ -4,6 +4,6 @@ test('test', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.getByRole('link', { name: 'Accessories Accessories' }).click();
   await page.getByRole('button').nth(2).click();
-  await page.getByRole('button', { name: 'Add to Cart' }).click();
+  await page.getByRole('button', { name: 'Add to Carted' }).click();
   await page.getByRole('button', { name: 'CLEAR CART' }).click();
 });
